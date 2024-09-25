3c8d8020170668ac2d02ffe078d25b3786a7a94d
flo <f@linguenheld.fr>
2024-09-25 17:12:07 +0200

    Add threads

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
 use std::{
     io::{prelude::*, BufReader},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread,
 };
 
 fn main() -> Result<()> {
@@ -10,56 +11,64 @@ fn main() -> Result<()> {
 
     let listener = TcpListener::bind("127.0.0.1:4221")?;
     for tcp_stream in listener.incoming() {
-        let mut stream = tcp_stream?;
+        println!("heps");
+        let stream = tcp_stream?;
         println!("accepted new connection");
 
-        let buf_reader = BufReader::new(&mut stream);
-        let mut it = buf_reader.lines();
+        thread::spawn(|| {
+            let _ = handle_connection(stream);
+        });
+    }
 
-        let mut content = String::new();
-        if let Some(raw_line) = it.next() {
-            let line = raw_line?;
+    Ok(())
+}
 
-            if line.starts_with("GET ") {
-                if let Some(requested_page) = line.split_whitespace().nth(1) {
-                    let code = match requested_page {
-                        p if p.starts_with("/echo/") => {
-                            content = (&p[6..]).to_string();
-                            200
-                        }
-                        _ if requested_page.starts_with("/user-agent") => {
-                            while let Some(raw_next_line) = it.next() {
-                                let next_line = raw_next_line?;
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let buf_reader = BufReader::new(&mut stream);
+    let mut it = buf_reader.lines();
 
-                                if next_line.contains("User-Agent:") {
-                                    content = (&next_line[12..]).to_string();
-                                    break;
-                                }
+    let mut content = String::new();
+    if let Some(raw_line) = it.next() {
+        let line = raw_line?;
+
+        if line.starts_with("GET ") {
+            if let Some(requested_page) = line.split_whitespace().nth(1) {
+                let code = match requested_page {
+                    p if p.starts_with("/echo/") => {
+                        content = p[6..].to_string();
+                        200
+                    }
+                    _ if requested_page.starts_with("/user-agent") => {
+                        for raw_next_line in it {
+                            let next_line = raw_next_line?;
+
+                            if next_line.contains("User-Agent:") {
+                                content = next_line[12..].to_string();
+                                break;
                             }
-
-                            200
                         }
-                        "/" => 200,
-                        _ => 400,
-                    };
 
-                    let response = match code {
-                        200 => {
-                            if content.is_empty() {
-                                "HTTP/1.1 200 OK\r\n\r\n"
-                            } else {
-                                &format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        200
+                    }
+                    "/" => 200,
+                    _ => 400,
+                };
+
+                let response = match code {
+                    200 => {
+                        if content.is_empty() {
+                            "HTTP/1.1 200 OK\r\n\r\n"
+                        } else {
+                            &format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
                                         content.len(), content)
-                            }
                         }
-                        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
-                    };
+                    }
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
 
-                    stream.write_all(response.as_bytes())?;
-                }
+                stream.write_all(response.as_bytes())?;
             }
         }
     }
-
     Ok(())
 }
