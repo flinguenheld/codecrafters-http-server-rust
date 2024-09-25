fec20f66f559ba4d9f6e42dd2d59224f14cf6d98
flo <f@linguenheld.fr>
2024-09-25 13:56:26 +0200

    Read lines on the fly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use std::{
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
+    net::TcpListener,
 };
 
 fn main() -> Result<()> {
@@ -10,59 +10,53 @@ fn main() -> Result<()> {
 
     let listener = TcpListener::bind("127.0.0.1:4221")?;
     for tcp_stream in listener.incoming() {
-        match tcp_stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
+        let mut stream = tcp_stream?;
+        println!("accepted new connection");
 
-                let buf_reader = BufReader::new(&mut stream);
-                let lines: Vec<String> = buf_reader
-                    .lines()
-                    .map(|result| result.unwrap())
-                    .take_while(|line| !line.is_empty())
-                    .collect();
-                let mut it = lines.iter();
+        let buf_reader = BufReader::new(&mut stream);
+        let mut it = buf_reader.lines();
 
-                let mut content: &str = "";
-                if let Some(first) = it.next() {
-                    if first.starts_with("GET ") {
-                        if let Some(page) = first.split_whitespace().nth(1) {
-                            let code = match page {
-                                p if p.starts_with("/echo/") => {
-                                    content = &p[6..];
-                                    200
-                                }
-                                _ if page.starts_with("/user-agent") => {
-                                    while let Some(line) = it.next() {
-                                        if line.contains("User-Agent:") {
-                                            content = &line[12..line.len()];
-                                        }
-                                    }
+        let mut content = String::new();
+        if let Some(raw_line) = it.next() {
+            let line = raw_line?;
 
-                                    200
-                                }
-                                "/" => 200,
-                                _ => 400,
-                            };
-
-                            let response = match code {
-                                200 => {
-                                    if content.is_empty() {
-                                        "HTTP/1.1 200 OK\r\n\r\n"
-                                    } else {
-                                        &format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                                    content.len(), content)
-                                    }
-                                }
-                                _ => "HTTP/1.1 404 Not Found\r\n\r\n",
-                            };
-
-                            stream.write_all(response.as_bytes())?;
+            if line.starts_with("GET ") {
+                if let Some(requested_page) = line.split_whitespace().nth(1) {
+                    let code = match requested_page {
+                        p if p.starts_with("/echo/") => {
+                            content = (&p[6..]).to_string();
+                            200
                         }
-                    }
+                        _ if requested_page.starts_with("/user-agent") => {
+                            while let Some(raw_next_line) = it.next() {
+                                let next_line = raw_next_line?;
+
+                                if next_line.contains("User-Agent:") {
+                                    content = (&next_line[12..]).to_string();
+                                    break;
+                                }
+                            }
+
+                            200
+                        }
+                        "/" => 200,
+                        _ => 400,
+                    };
+
+                    let response = match code {
+                        200 => {
+                            if content.is_empty() {
+                                "HTTP/1.1 200 OK\r\n\r\n"
+                            } else {
+                                &format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                                        content.len(), content)
+                            }
+                        }
+                        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                    };
+
+                    stream.write_all(response.as_bytes())?;
                 }
-            }
-            Err(e) => {
-                println!("error: {}", e);
             }
         }
     }
