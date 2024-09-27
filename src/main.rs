4bcc1dcf7bd5dbe6b4aa97f32c2685260eaef5d3
flo <f@linguenheld.fr>
2024-09-27 13:25:18 +0200

    Up bufreader

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use anyhow::Result;
 use std::{
-    env, fs,
-    io::{prelude::*, BufReader},
+    env,
+    fs::{self},
+    io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
     path::Path,
     thread::{self, JoinHandle},
@@ -30,16 +31,24 @@ fn main() -> Result<()> {
 }
 
 fn handle_connection(mut stream: TcpStream, file_dir: String) -> Result<()> {
-    let buf_reader = BufReader::new(&mut stream);
-    let mut it = buf_reader.lines();
+    let mut buf_reader = BufReader::new(&mut stream);
+    let received = buf_reader.fill_buf()?;
+    let request_string = String::from_utf8_lossy(received).into_owned();
 
+    // Also ok -----
+    // let mut request = [0_u8; 1024];
+    // let bytes = stream.read(&mut request)?;
+    // let request_string = String::from_utf8_lossy(&request[..bytes]).into_owned();
+    // -----
+
+    let mut it_lines = request_string.lines();
+
+    let mut code = 404;
     let mut message = Vec::new();
-    if let Some(raw_line) = it.next() {
-        let line = raw_line?;
-
+    if let Some(line) = it_lines.next() {
         if line.starts_with("GET ") {
             if let Some(requested_page) = line.split_whitespace().nth(1) {
-                let code = match requested_page {
+                code = match requested_page {
                     p if p.starts_with("/echo/") => {
                         message.push("Content-Type: text/plain".to_string());
                         message.push(format!("Content-Length: {}\r\n", p[6..].len()));
@@ -47,9 +56,7 @@ fn handle_connection(mut stream: TcpStream, file_dir: String) -> Result<()> {
                         200
                     }
                     _ if requested_page.starts_with("/user-agent") => {
-                        for raw_next_line in it {
-                            let next = raw_next_line?;
-
+                        for next in it_lines {
                             if next.contains("User-Agent:") {
                                 message.push("Content-Type: text/plain".to_string());
                                 message.push(format!("Content-Length: {}\r\n", &next[12..].len()));
@@ -77,15 +84,39 @@ fn handle_connection(mut stream: TcpStream, file_dir: String) -> Result<()> {
                     "/" => 200,
                     _ => 404,
                 };
+            }
+        } else if line.starts_with("POST /files/") {
+            let mut length = 0;
+            let mut content = String::new();
 
-                let response = match code {
-                    200 => &format!("HTTP/1.1 200 OK\r\n{}\r\n", message.join("\r\n")),
-                    _ => &format!("HTTP/1.1 404 Not Found\r\n{}\r\n", message.join("\r\n")),
-                };
+            if let Some((file_name, _)) = &line[12..].split_once(" ") {
+                for con in it_lines {
+                    content = con.to_string(); // File's content is at the end of the request
 
-                stream.write_all(response.as_bytes())?;
+                    if content.starts_with("Content-Length: ") {
+                        dbg!(&content);
+                        if let Ok(value) = &content[16..].parse::<usize>() {
+                            length = *value;
+                        }
+                    }
+                }
+
+                if length == content.len() && !file_name.is_empty() {
+                    let path = format!("{}{}", file_dir, file_name);
+                    fs::create_dir_all(file_dir)?;
+                    fs::write(path, content)?;
+
+                    code = 201;
+                }
             }
         }
+
+        let response = match code {
+            200 => format!("HTTP/1.1 200 OK\r\n{}\r\n", message.join("\r\n")),
+            201 => "HTTP/1.1 201 Created\r\n\r\n".to_string(),
+            _ => format!("HTTP/1.1 404 Not Found\r\n{}\r\n", message.join("\r\n")),
+        };
+        stream.write_all(response.as_bytes())?;
     }
     Ok(())
 }
@@ -109,7 +140,7 @@ impl ThreadPool {
         if self.currents.len() < self.maxi {
             println!("Connection accepted");
             self.currents
-                .push(thread::spawn(|| handle_connection(stream, file_dir)));
+                .push(thread::spawn(move || handle_connection(stream, file_dir)));
         } else {
             println!("Connection refused");
         }
